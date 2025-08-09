116cfc4a11845ceacc8e7bcecc2256afd316cab6
AimotoRyosuke <r.aimoto97vg@gmail.com>
2025-08-09 23:50:12 +0900

    apply lint rule

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,29 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/// アプリケーションのエントリーポイント
 void main() {
-  runApp(const MyApp());
+  runApp(
+    // Riverpodをアプリケーション全体で利用可能にする
+    const ProviderScope(child: MyApp()),
+  );
 }
 
+/// アプリケーションのルートWidget
 class MyApp extends StatelessWidget {
+  /// {@macro my_app}
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '花火の距離',
+      title: 'Hanabi Distance',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: '花火の距離'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool _isRunning = false;
-  DateTime? _startTime;
-  double _distance = 0.0;
-  Timer? _timer;
-
-  void _startTimer() {
-    setState(() {
-      _isRunning = true;
-      _startTime = DateTime.now();
-      _distance = 0.0;
-    });
-  }
-
-  void _stopTimer() {
-    if (_isRunning) {
-      final endTime = DateTime.now();
-      final difference = endTime.difference(_startTime!);
-      final seconds = difference.inMilliseconds / 1000.0;
-      setState(() {
-        _distance = seconds * 340.0;
-        _isRunning = false;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '${_distance.toStringAsFixed(2)} m',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: _startTimer,
-            tooltip: 'Start',
-            child: const Icon(Icons.play_arrow),
-          ),
-          const SizedBox(height: 16),
-          FloatingActionButton(
-            onPressed: _stopTimer,
-            tooltip: 'Stop',
-            child: const Icon(Icons.stop),
-          ),
-        ],
-      ),
+      // TODO(user): ここで起動時の画面出し分けロジックを実装する
+      home: const Scaffold(body: Center(child: Text('Hanabi Distance App'))),
     );
   }
 }
